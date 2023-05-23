3da1398123793c652ff1dc1d0a50d74bcc21ef02
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2023-05-23 22:08:56 +0200

    fetched and stored product in state

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react"
+
+const url = "https://api.noroff.dev/api/v1/online-shop";
+
+
 function Home () {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+    const response = await fetch(url)
+    const data = await response.json()
+    setProducts(data)
+    }
+    getProducts()
+  }, [])
+  console.log(products)
   return (
     <div className="home">
       <h1>Home</h1>
