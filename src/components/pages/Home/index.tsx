612029125bbe20a51226d9a03856aee040c26c6c
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2023-05-23 23:57:44 +0200

    looped trough products and displayed info

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,23 +1,34 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const url = "https://api.noroff.dev/api/v1/online-shop";
+const url = 'https://api.noroff.dev/api/v1/online-shop'
 
-
-function Home () {
-  const [products, setProducts] = useState([]);
+function Home() {
+  const [posts, setPosts] = useState<any>([])
 
   useEffect(() => {
-    async function getProducts() {
-    const response = await fetch(url)
-    const data = await response.json()
-    setProducts(data)
+    async function getData() {
+      const response = await fetch(url)
+      const json = await response.json()
+      setPosts(json)
     }
-    getProducts()
+    getData()
   }, [])
-  console.log(products)
+
   return (
-    <div className="home">
-      <h1>Home</h1>
+    <div>
+      {posts.map((post: any) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.description}</p>
+          <p>{post.price}</p>
+          <p>rating: {post.rating}</p>
+          <img src={post.imageUrl} alt={post.title} />
+          <Link to={`/product/${post.id}`}>
+            <button>View product</button>
+          </Link>
+        </div>
+      ))}
     </div>
   )
 }
