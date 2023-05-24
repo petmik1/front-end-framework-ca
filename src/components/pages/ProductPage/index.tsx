04b30cabfa3c4ca90162bb461b9169ac3d5b04a5
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2023-05-25 00:50:59 +0200

    worked on product page

--- src/components/pages/ProductPage/index.tsx
+++ src/components/pages/ProductPage/index.tsx
@@ -1,11 +1,31 @@
-import { useParams } from "react-router-dom";
+import { log } from 'console'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+const url = 'https://api.noroff.dev/api/v1/online-shop'
 
 function ProductPage() {
-  let { id } = useParams();
+  const { id } = useParams()
+  const [product, setProduct] = useState<any>([])
 
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(url + '/' + id)
+      const json = await response.json()
+      setProduct(json)
+    }
+    getData()
+  }, [])
+
+  function addToCart() {
+    console.log('add to cart')
+  }
   return (
     <div>
-      <h1>ProductPage</h1>
+      <h1>{product.title}</h1>
+      <p>{product.description}</p>
+      <img src={product.imageUrl} alt={product.title} />
+      <button onClick={addToCart}>add to cart</button>
     </div>
   )
 }
