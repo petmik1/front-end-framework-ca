925d110d0d8217a650d4d8a0fe027d6682d00bf1
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2023-05-28 01:25:27 +0200

    added yup for error handeling

--- src/pages/ContactPage/index.tsx
+++ src/pages/ContactPage/index.tsx
@@ -1,4 +1,6 @@
 import { SubmitHandler, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 type FormValues = {
   fullName: string
@@ -7,9 +9,17 @@ type FormValues = {
   message: string
 }
 
-function ContactPage() {
-  const { register, handleSubmit } = useForm<FormValues>()
+const schema = yup.object().shape({
+  fullName: yup.string().min(3).required(),
+  email: yup.string().email().required(),
+  subject: yup.string().min(3).required(),
+  message: yup.string().min(3).required(),
+})
 
+
+function ContactPage() {
+  const { register, handleSubmit, formState: {errors} } = useForm<FormValues>({resolver: yupResolver(schema)})
+  
   const test: SubmitHandler<FormValues> = (data) => console.log(data)
 
   return (
@@ -17,18 +27,22 @@ function ContactPage() {
       <div>
         <label htmlFor="fullName">full Name</label>
         <input {...register('fullName')} />
+        <p>{errors.fullName?.message}</p>
       </div>
       <div>
         <label htmlFor="email">email</label>
         <input {...register('email')} />
+        <p>{errors.email?.message}</p>
       </div>
       <div>
         <label htmlFor="subject">subject</label>
         <input {...register('subject')} />
+        <p>{errors.subject?.message}</p>
       </div>
       <div>
         <label htmlFor="message">message</label>
         <input {...register('message')} />
+        <p>{errors.message?.message}</p>
       </div>
       <input type="submit" />
     </form>
