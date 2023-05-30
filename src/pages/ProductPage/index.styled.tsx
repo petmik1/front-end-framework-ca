47a0e6a04d13d24269801504de95beeb9ea42fc5
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2023-05-30 09:30:01 +0200

    added styling for product page

--- src/pages/ProductPage/index.styled.tsx
+++ src/pages/ProductPage/index.styled.tsx
@@ -4,7 +4,7 @@ export const ProductPageStyled = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 100vh;
+  height: 80vh;
 
   @media (min-width: 768px) {
     flex-direction: row;
